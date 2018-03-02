b0d1d8f82caebcaa781f34c620ae7a816b65dc75
johnny <johnny@wantgoo.com>
2018-03-02 16:05:04 +0800

    從 iOS8 開始無法在實機上取得 Serial Number

--- DeviceIdApp/DeviceIdApp.iOS/Services/IOSDeviceService.cs
+++ DeviceIdApp/DeviceIdApp.iOS/Services/IOSDeviceService.cs
@@ -12,6 +12,9 @@ namespace DeviceIdApp.iOS.Services
     {
         public string GetDeviceId()
         {
+            return UIKit.UIDevice.CurrentDevice.IdentifierForVendor.AsString();
+
+            // 從 iOS8 開始無法在實機上取得 Serial Number
             var serial = string.Empty;
             var platformExpert = IOServiceGetMatchingService(0, IOServiceMatching("IOPlatformExpertDevice"));
             if (platformExpert == 0)
